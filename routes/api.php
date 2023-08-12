6566cc532988ca588abdc0103c829498a077b22b
roihan365 <bitcoiner.banjarmasin@gmail.com>
2023-08-12 14:08:17 +0800

    y

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register',[AuthController::class, 'register']);
-Route::post('/login',[AuthController::class, 'login']);
+Route::post('/login',[AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout',[AuthController::class, 'logout']);

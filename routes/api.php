995def3479427453babc16b234049a1d8e860805
roihan365 <bitcoiner.banjarmasin@gmail.com>
2023-08-22 15:39:53 +0800

    integrasi web admin

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register',[AuthController::class, 'register']);
-Route::post('/login',[AuthController::class, 'login'])->name('login');
+Route::post('/login',[AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout',[AuthController::class, 'logout']);
